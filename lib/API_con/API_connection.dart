ae4fac67385fd490eee64ac01b5338a26dc614fd
OstapFilipenko_mea <ostap.filipenko@mea-it.services>
2020-04-25 18:54:44 +0200

    deleted the params

--- lib/API_con/API_connection.dart
+++ lib/API_con/API_connection.dart
@@ -2,15 +2,10 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 
 class API_connection{
-  static var params = {
-    'per_page': '30',
-  };
 
-  static var uri =  Uri.https('api.unsplash.com', '/photos', params);
-
-  static Future getPictures(){
-    return http.get(
-      uri,
+  static Future getPictures() async{
+    return await http.get(
+      Uri.encodeFull("https://api.unsplash.com//photos?per_page=30"),
       headers: {
         'Accept-Version': 'v1',
         'Authorization': 'Client-ID fZZop4D9xtDfIsLvMVHAn8D9DAWoA0h4T2mqXJOUAkU',
